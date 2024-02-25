1b622f8b53809380bc9ee9ee5873ccee329d58cf
Sam <138595608+AIWizard47@users.noreply.github.com>
2024-02-25 17:13:54 +0530

    Update urls.py

--- project1/urls.py
+++ project1/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include ,re_path
 from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('beta/v2/admin-sam/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('',include('user.urls')),
